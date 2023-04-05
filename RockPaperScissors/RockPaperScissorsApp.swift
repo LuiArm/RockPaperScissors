94cf3bec73893fffe323e526c1943a2498ddef9c
LuiArm <Luisarmendariz24@icloud.com>
2023-04-05 09:59:15 -0600

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by luis armendariz on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
